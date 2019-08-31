6abda07b974becf0319588f6e769c7d27a565fa3
norvca <norvca@gmail.com>
2019-08-31 11:38:17 +0800

    Set template engine

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.render("home-guest");
 });
 
 app.listen(3000);
