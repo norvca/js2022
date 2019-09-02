b777d1ee4bff1c267fda3774ea0f0ccbb17bc4eb
norvca <norvca@gmail.com>
2019-09-02 09:09:32 +0800

    Add environment variables

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const mongodb = require("mongodb");
 
-const connectionString =
-  "mongodb+srv://testman:test2345@project-ugt6y.mongodb.net/js2022?retryWrites=true&w=majority";
-
 mongodb.connect(
-  connectionString,
+  process.env.connectionstring,
   { useNewUrlParser: true, useUnifiedTopology: true },
   function(err, client) {
     module.exports = client.db();
     const app = require("./app");
-    app.listen(3000);
+    app.listen(process.env.PORT);
   }
 );
